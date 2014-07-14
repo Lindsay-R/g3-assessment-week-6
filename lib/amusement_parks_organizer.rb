0ec6dba9f80cd5e58cdb3b65ee632398cbb8c4bd
Lindsay-R <lrecknell@gmail.com>
2014-07-14 10:25:27 -0600

    times up

---
+++ lib/amusement_parks_organizer.rb
@@ -0,0 +1,19 @@
+class AmusementParksOrganizer
+
+  def initialize(id_array, country_array)
+    @park_id = id_array
+    @park_country = country_array
+  end
+
+  def create_a_id_hash
+
+    @park_id.map do |x|
+     {x[:id]=> x }
+    end
+  end
+
+  def create_a_country_hash
+
+  end
+
+end #class end
--- spec/amusement_parks_organizer_spec.rb
+++ spec/amusement_parks_organizer_spec.rb
@@ -1,8 +1,161 @@
-require "lib/amusement_parks_organizer"
+require "amusement_parks_organizer"
+
+
 
 describe AmusementParksOrganizer do
 
+  before do
+    @park_id =[
+      {
+        :id=>546,
+        :name=>"Kalahari Resorts",
+        :city=>"Wisconsin Dells",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>547,
+        :name=>"Little Amerricka",
+        :city=>"Marshall",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      }
+    ]
+
+    @park_country = [
+      {
+        :id=>546,
+        :name=>"Kalahari Resorts",
+        :city=>"Wisconsin Dells",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>547,
+        :name=>"Little Amerricka",
+        :city=>"Marshall",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      {
+        :id=>2,
+        :name=>"Calaway Park",
+        :city=>"Calgary",
+        :state=>"Alberta",
+        :country=>"Canada"
+      }
+    ]
+
+  end  #before do, end
 
 
+describe "#create_an_id_hash" do
+  it "returns a hash of from id array" do
+  {
+    546 => {
+      :id=>546,
+      :name=>"Kalahari Resorts",
+      :city=>"Wisconsin Dells",
+      :state=>"Wisconsin",
+      :country=>"United States"
+    },
+    547 => {
+      :id=>547,
+      :name=>"Little Amerricka",
+      :city=>"Marshall",
+      :state=>"Wisconsin",
+      :country=>"United States"
+    }
+  }
+
+  expect(AmusementParksOrganizer.new(@park_id, @park_country).create_an_id_hash).to eq(
+    {
+      546 => {
+        :id=>546,
+        :name=>"Kalahari Resorts",
+        :city=>"Wisconsin Dells",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      },
+      547 => {
+        :id=>547,
+        :name=>"Little Amerricka",
+        :city=>"Marshall",
+        :state=>"Wisconsin",
+        :country=>"United States"
+      }
+    }
+  )
+
+  end
 end
+
+describe "#create_a_country_hash" do
+  it "returns a hash of from country array" do
+
+    {
+      "Canada" => [
+        {
+          :id=>2,
+          :name=>"Calaway Park",
+          :city=>"Calgary",
+          :state=>"Alberta",
+          :country=>"Canada"
+        }
+      ],
+      "United States" => [
+        {
+          :id=>546,
+          :name=>"Kalahari Resorts",
+          :city=>"Wisconsin Dells",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        },
+        {
+          :id=>547,
+          :name=>"Little Amerricka",
+          :city=>"Marshall",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        }
+      ]
+    }
+
+    expect(AmusementParksOrganizer.new(@park_id, @park_country).create_a_country_hash).to eq(
+    {
+      "Canada" => [
+        {
+          :id=>2,
+          :name=>"Calaway Park",
+          :city=>"Calgary",
+          :state=>"Alberta",
+          :country=>"Canada"
+        }
+      ],
+      "United States" => [
+        {
+          :id=>546,
+          :name=>"Kalahari Resorts",
+          :city=>"Wisconsin Dells",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        },
+        {
+          :id=>547,
+          :name=>"Little Amerricka",
+          :city=>"Marshall",
+          :state=>"Wisconsin",
+          :country=>"United States"
+        }
+      ]
+    }
+  )
+  end
+end
+
+
+
+
+end #describe end
+
 
